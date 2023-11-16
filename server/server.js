4907222dc551e4387d04af7d50de6bc2051c1858
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-11-16 18:54:10 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const bodyParser = require('body-parser');
 
+const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
+mongoose.connect(mongoUri)
+
 // PARSING
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 
 
