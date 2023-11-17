89c34cada948a7bb1c06c2d18843163e72603679
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-11-17 19:09:10 +0100

    Controllers

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+
+
+
+
+const authController = {
+    async register(req,res,next){
+        try{
+            /////
+        } catch(error){
+            ///
+        }
+    },
+}
+
+
+module.exports = authController;
