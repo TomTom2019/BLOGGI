d2d2f7310b63b5abb594973af119e34d52f5d6da
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-12-10 19:09:08 +0100

    first install + header + layout

---
+++ frontend/src/routes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+
+
+import MainLayout from './hoc/mainLayout';
+import Home from './components/home';
+import Header from './components/navigation/header';
+
+
+const Router = () => {
+  return(
+    <BrowserRouter>
+    <Header/>
+    <MainLayout>
+      <Routes>
+         <Route path='/' element={<Home/>}/>
+      </Routes>
+      </MainLayout>
+    </BrowserRouter>
+  )
+}
+
+export default Router
