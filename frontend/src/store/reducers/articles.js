82ecb050d91e26ecaf6358e26bcbab614e0f0fa8
Tom <45665177+TomTom2019@users.noreply.github.com>
2023-12-11 19:13:54 +0100

    redux

---
+++ frontend/src/store/reducers/articles.js
@@ -0,0 +1,14 @@
+
+
+import { createSlice } from '@reduxjs/toolkit';
+
+export const articlesSlice = createSlice({
+    name:'articles',
+    initialState:{},
+    reducers:{
+
+    }
+});
+
+/// action......
+export default articlesSlice.reducer;
