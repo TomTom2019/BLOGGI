608e41f1b91d9428345096b2ce92101ec22223b1
Tom <45665177+TomTom2019@users.noreply.github.com>
2024-01-11 19:12:29 +0100

    deploy

--- server/server.js
+++ server/server.js
@@ -40,6 +40,15 @@ app.use((err,req,res,next)=>{
     handleError(err,res)
 });
 
+//HEROKU
+app.use(express.static('client/build'));
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'../client','build','index.html'))
+    });
+}
+
 
 const port =  process.env.PORT || 3001;
 app.listen(port,()=>{
